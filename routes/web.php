975cf84757a926504b051b5a10b333ee6220b989
Andrea <ing.orlandi.andrea@gmail.com>
2018-10-17 17:27:06 +0200

    yet another try to deploy https on heroku, quater

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,13 @@ Route::get('.hello', function () {
     return 'world';
 });
 
+Route::get('/.hi', function () {
+    return 'tada';
+});
+
+Route::get('world', function () {
+    return 'hello';
+});
 
 Route::get('.well-known/acme-challenge/5XbncyfGkOAZizeedP7FW59qXddARJOqKKKT9NnwFnY', function () {
     return '5XbncyfGkOAZizeedP7FW59qXddARJOqKKKT9NnwFnY.afiEOhroSFWLh0vH7GO6GHJt3sWscU1IxaDZ1JLL6kU';
